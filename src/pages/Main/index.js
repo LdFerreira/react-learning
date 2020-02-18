4bb9095ba69b9209bd5e109b33ef94c34e72e326
Leandro Ferreira <leandroferreira@cliente-200-135-90-34.wireless.ufsc.br>
2020-02-18 19:34:43 -0300

    added List in localstorage and routes navegation

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 import api from '../../services/api';
 import { Container, Form, SubmitButton, List } from './styles';
@@ -10,6 +11,25 @@ export default class Main extends Component {
     repositories: [],
     loading: false,
   };
+
+  // Carregar os dados do localstorage
+  componentDidMount() {
+    const repositories = localStorage.getItem('repositories');
+
+    if (repositories) {
+      this.setState({
+        repositories: JSON.parse(repositories),
+      });
+    }
+  }
+
+  // Salvar os dados do localstorage
+  componentDidUpdate(_, prevState) {
+    const { repositories } = this.state;
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('repositories', JSON.stringify(repositories));
+    }
+  }
 
   handleInputChange = e => {
     this.setState({ newRepo: e.target.value });
@@ -63,7 +83,9 @@ export default class Main extends Component {
           {repositories.map(repository => (
             <li key={repository.nome}>
               <span>{repository.nome}</span>
-              <a href="">Detalhes</a>
+              <Link to={`/repository/${encodeURIComponent(repository.nome)}`}>
+                Detalhes
+              </Link>
             </li>
           ))}
         </List>
